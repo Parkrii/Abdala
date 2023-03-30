c467ef7f89bb55456fb10a14cdf7873a4515c2ee
Nicolás Luna <102234970+Parkrii@users.noreply.github.com>
2023-03-30 18:14:05 -0300

    EsGrito

---
+++ EsGrito/JS/funcion.js
@@ -0,0 +1,17 @@
+function grito(m){
+    let grito=0, letra=0;
+    for(let i=0; i<m.length; i++){
+    	if(m[i] == '!'){
+    		grito++;
+    	}
+    	if(isNaN(m[i]) && m[i] != '!'){
+    		letra++;
+    	}
+    }
+    if(grito>letra){
+    	return "ESGRITO";
+    }
+    else{
+    	return "escrito";
+    }
+}
