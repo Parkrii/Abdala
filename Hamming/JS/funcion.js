c165afb8a901870e1ad8072731b036be3968d5ce
Nicolás Luna <102234970+Parkrii@users.noreply.github.com>
2023-04-28 19:27:57 -0300

    Hamming

--- Hamming/JS/funcion.js
+++ Hamming/JS/funcion.js
@@ -2,7 +2,6 @@ function Hamming(b, b2, largo) {
     let hamming = 0;
 
     for (let i = 0; i < largo; i++) {
-        console.log(b[i], b2[i])
         if (b[i] != b2[i]) {
             hamming++;
         }
