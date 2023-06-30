8ebaf81bb20e279429cc576aef16c146d0d706c5
Nicolás Luna <102234970+Parkrii@users.noreply.github.com>
2023-06-30 14:06:05 -0300

    Class Repaso

---
+++ Js ClassRepaso/js/functions.js
@@ -0,0 +1,28 @@
+// class.js
+
+class CRectangulo {
+  constructor(altura, longitud) {
+    this.altura = altura;
+    this.longitud = longitud;
+  }
+
+  mostrarArea() {
+    return `La area es: ${this.altura * this.longitud}`;
+  }
+}
+
+const formulario = document.getElementById('formulario');
+formulario.addEventListener('submit', function(event)
+
+ {
+  event.preventDefault();
+
+  const altura = document.getElementById('altura').value;
+  const longitud = document.getElementById('longitud').value;
+
+  const rectangulo = new CRectangulo(altura, longitud);
+
+  const areaRectangulo = document.getElementById('areaRectangulo');
+  areaRectangulo.innerHTML = rectangulo.mostrarArea();
+ }
+);
